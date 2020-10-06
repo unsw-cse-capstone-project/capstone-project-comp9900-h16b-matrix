ec1a33a6fa0d57b3ab78050cf0ef1228e1fabc9d
charles0801 <wqypoiu@live.cn>
2020-10-06 17:25:13 +0800

    java spring boot project initialized

---
+++ filmfinder/src/main/java/com/matrix/filmfinder/FilmfinderApplication.java
@@ -0,0 +1,13 @@
+package com.matrix.filmfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmfinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmfinderApplication.class, args);
+	}
+
+}
