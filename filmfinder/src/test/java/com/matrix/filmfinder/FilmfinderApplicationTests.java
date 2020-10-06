0cab241d9ef9b3a1ef7de4c4ef4b018fada40dbb
charles0801 <wqypoiu@live.cn>
2020-10-06 17:25:13 +0800

    java spring boot project initialized

---
+++ filmfinder/src/test/java/com/matrix/filmfinder/FilmfinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matrix.filmfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmfinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
