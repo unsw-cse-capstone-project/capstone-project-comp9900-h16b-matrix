07b6cdd36feff92e10497a09531b95c9d22ff22b
charles0801 <wqypoiu@live.cn>
2020-10-12 21:10:55 +0800

    Finish basic UserController

--- filmfinder/src/test/java/com/matrix/filmfinder/FilmfinderApplicationTests.java
+++ filmfinder/src/test/java/com/matrix/filmfinder/FilmfinderApplicationTests.java
@@ -1,13 +1,26 @@
 package com.matrix.filmfinder;
 
+import com.matrix.filmfinder.model.Movie;
+import com.matrix.filmfinder.model.User;
+import org.hibernate.Session;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.transaction.UserTransaction;
+import java.util.UUID;
 
 @SpringBootTest
 class FilmfinderApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void addUsers() {
+
 	}
 
 }
