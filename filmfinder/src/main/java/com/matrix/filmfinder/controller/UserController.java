68f9a84322e4d2e1cd3d5cc91d10dd7d5f62d93f
charles0801 <wqypoiu@live.cn>
2020-10-12 21:10:55 +0800

    Finish basic UserController

--- filmfinder/src/main/java/com/matrix/filmfinder/controller/UserController.java
+++ filmfinder/src/main/java/com/matrix/filmfinder/controller/UserController.java
@@ -1,5 +1,28 @@
 package com.matrix.filmfinder.controller;
 
-public class UserController {
+import com.matrix.filmfinder.dao.UserRepository;
+import com.matrix.filmfinder.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
+@Controller
+@RequestMapping(path="/demo")
+public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping(path="/add")
+    public @ResponseBody String addNewUSer(@RequestParam String name, @RequestParam String email) {
+        User user = new User();
+        user.setName(name);
+        user.setEmail(email);
+        userRepository.save(user);
+        String res = "User " + user.toString() + " saved.";
+        return res;
+    }
+    @GetMapping(path="/all")
+    public @ResponseBody Iterable<User> getAllUsers() {
+        return userRepository.findAll();
+    }
 }
