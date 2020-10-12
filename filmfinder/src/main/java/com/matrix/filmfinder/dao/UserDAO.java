466cda7a9f23a55d4ced4a3c4fb3d69fbea28a55
charles0801 <wqypoiu@live.cn>
2020-10-12 15:48:50 +0800

    add User and Movie entities

---
+++ filmfinder/src/main/java/com/matrix/filmfinder/dao/UserDAO.java
@@ -0,0 +1,12 @@
+package com.matrix.filmfinder.dao;
+
+import com.matrix.filmfinder.model.User;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserDAO {
+//    User save(User user);
+//    Optional<User> findById(Integer Id);
+//    List<User>v
+}
