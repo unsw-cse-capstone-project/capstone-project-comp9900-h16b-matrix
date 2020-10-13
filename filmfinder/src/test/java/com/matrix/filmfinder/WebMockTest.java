3b5b7eff8dc0c9493b734bd0b2f5b8f1fd702c0a
charles0801 <wqypoiu@live.cn>
2020-10-13 13:42:07 +0800

    Writed some Junit tests

--- filmfinder/src/test/java/com/matrix/filmfinder/WebMockTest.java
+++ filmfinder/src/test/java/com/matrix/filmfinder/WebMockTest.java
@@ -3,6 +3,7 @@ package com.matrix.filmfinder;
 
 import com.matrix.filmfinder.controller.UserController;
 import com.matrix.filmfinder.model.User;
+import org.codehaus.jackson.map.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -11,23 +12,32 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(UserController.class)
 public class WebMockTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
-    private User user;
+
+    @Test
+    public void contextLoads(){
+
+    }
 
     @Test
     public void addUserTest() throws Exception {
-        User u = new User();
-        u.setName("charles");
-        u.setEmail("charles@sample.com");
+        User u = new User("charles", "charles@sample.com");
+        ObjectMapper om = new ObjectMapper();
         //TODO
-//        mockMvc.perform(post("/demo/add")
-//                .contentType(MediaType.APPLICATION_JSON)
-//                .content())
+        String responseString = mockMvc.perform(
+                post("/demo/add")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(om.writeValueAsString(u))
+        ).andExpect(status().isOk())
+                .andDo(print())
+                .andReturn().getResponse().getContentAsString();
+        System.out.println(responseString);
     }
 }
