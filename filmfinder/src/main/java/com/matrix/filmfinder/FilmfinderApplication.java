aad49772e4dae21ef76f7a8fcd2b971cc5fb95c0
charles0801 <wqypoiu@live.cn>
2020-10-14 20:41:41 +0800

    Very drafted version of login/signup

--- filmfinder/src/main/java/com/matrix/filmfinder/FilmfinderApplication.java
+++ filmfinder/src/main/java/com/matrix/filmfinder/FilmfinderApplication.java
@@ -1,10 +1,34 @@
 package com.matrix.filmfinder;
 
+import com.matrix.filmfinder.model.User;
+import com.matrix.filmfinder.services.FilmFinderUserDetailService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
+
+import java.util.Arrays;
+import java.util.Collection;
 
 @SpringBootApplication
 public class FilmfinderApplication {
+
+//    @Bean
+//    PasswordEncoder passwordEncoder() {
+//        PasswordEncoder myPasswordEncoder = new SCryptPasswordEncoder();
+//        return myPasswordEncoder;
+//    }
+//
+//    @Bean
+//    void ffUserDetailService() {
+//        Collection<User> users = Arrays.asList(
+//                new User("Alice", "Alice@sample.com", "HaHaHa", "USER"),
+//                new User("Bob", "Bob@sample.com", "123123", "USER")
+//        );
+//
+//    }
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(FilmfinderApplication.class, args);
---
+++ filmfinder/src/main/java/com/matrix/filmfinder/util/JwtUtil.java
@@ -0,0 +1,5 @@
+package com.matrix.filmfinder.util;
+
+public class JwtUtil {
+    
+}
