ed214a390d9062f5a8942d30e14bee78231d960e
Jiawen <jiawen.huang0323@gmail.com>
2020-10-31 21:20:53 +0800

    update

--- filmfinder/src/main/java/com/matrix/filmfinder/controller/RateController.java
+++ filmfinder/src/main/java/com/matrix/filmfinder/controller/RateController.java
@@ -6,10 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path="/rate")
@@ -18,7 +15,7 @@ public class RateController {
     private RateRepository rateRepository;
 
 //    add
-    @PostMapping(path="/rate/add")
+    @PostMapping(path="/addrate")
     public Rate addRate(
             @RequestParam(name = "uid") Integer uid,
             @RequestParam(name = "movie_id") Integer movie_id,
@@ -33,10 +30,9 @@ public class RateController {
     }
 
     //    update
-    @Modifying(clearAutomatically = true)
-    @Query("update Rate a set a.rating=?3 where a.uid=?1 and a.movie_id=?2")
-    int updateRate(@Param("uid") Integer uid, @Param("movie_id") Integer movie_id, @Param("rating") Integer rating)
-    {
-        return 0;//???????????
+    @PutMapping(value = "udrate/{uid}&{movie_id}")
+    public void updateRate(@PathVariable Integer uid){
+//        Rate rate = rateRepository.findById(id).get();
+
     }
 }
