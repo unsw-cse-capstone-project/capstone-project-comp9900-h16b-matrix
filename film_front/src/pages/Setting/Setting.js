9438fc5aed6db7cf14284a31aaae3cdaf425c7d5
z5201868 <z5201868@cse.unsw.edu.au>
2020-11-01 19:00:55 +1100

    update setting

--- film_front/src/pages/Setting/Setting.js
+++ film_front/src/pages/Setting/Setting.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 import Tabs from '@material-ui/core/Tabs';
@@ -9,8 +9,16 @@ import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import Divider from '@material-ui/core/Divider';
-import Chip from '@material-ui/core/Chip';
 import { FaUserCircle } from "react-icons/fa";
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import ListItemText from '@material-ui/core/ListItemText';
+import Avatar from '@material-ui/core/Avatar';
+import DeleteIcon from '@material-ui/icons/Delete';
+import IconButton from '@material-ui/core/IconButton';
+import NavBar from '../NavBar/NavBar'
 
 
 function TabPanel(props) {
@@ -46,6 +54,7 @@ function a11yProps(index) {
   };
 }
 
+
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
@@ -62,121 +71,162 @@ export default function VerticalTabs() {
   const classes = useStyles();
   const [value, setValue] = React.useState(0);
 
+  const [userInfo, setUserInfo] = useState({
+    'Old password': "",
+    'New password': "",
+    'Confirm New Password': ""
+  })
+
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
 
-  const handleDelete = () => {
-     console.info('You clicked the delete icon.');
+  const handleDelete = (chipToDelete) => () => {
+    setChipData((chips) => chips.filter((chip) => chip.key !== chipToDelete.key));
   };
-  
+
   const handleClick = () => {
     console.info('You clicked the Chip.');
   };
 
+  const passwordChange = (e) => {
+    setUserInfo(
+      {
+        ...userInfo,
+        [e.target.name]: e.target.value
+      }
+    )
+  }
+
+  const [chipData, setChipData] = React.useState([
+    { key: 0, label: 'Anna' },
+    { key: 1, label: 'Becky' },
+    { key: 2, label: 'Charlie' },
+    { key: 3, label: 'Denny' },
+    { key: 4, label: 'Flora' },
+  ]);
+
+  const [dense, setDense] = React.useState(false);
+
+  const [open, setOpen] = useState(false);
+  const [SignupOpen, SignupsetOpen] = useState(false);
+  const [logout, setLogout] = useState(false)
+  const rederLogout = () => setLogout(!logout)
+  const handleSignupOpen = () => {
+    SignupsetOpen(true);
+    handleClose();
+  };
+  const SignupClose = () => {
+    SignupsetOpen(false);
+  };
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
 
 
-  
+
 
   return (
     <div className={classes.root}>
-      <Tabs
-        orientation="vertical"
-        variant="scrollable"
-        value={value}
-        onChange={handleChange}
-        aria-label="Vertical tabs example"
-        className={classes.tabs}
-      >
-        <Tab label="Change password" {...a11yProps(0)} />
-        <Tab label="Banlist management" {...a11yProps(1)} />
-        
-      </Tabs>
-
       <Grid container spacing={3} justify='center'>
-        <Grid item xs={5}>
+        <Grid item xs={12} >
+          <NavBar handleClose={handleClose}
+            open={open}
+            handleClickOpen={handleClickOpen}
+            SignupClose={SignupClose}
+            SignupOpen={SignupOpen}
+            handleSignupOpen={handleSignupOpen}
+            rederLogout={rederLogout}
+          />
+        </Grid>
+
+        <Grid item xs={2} >
+          <Tabs
+            orientation="vertical"
+            variant="scrollable"
+            value={value}
+            onChange={handleChange}
+            aria-label="Vertical tabs example"
+            className={classes.tabs}
+          >
+            <Tab label="Change password" {...a11yProps(0)} />
+            <Tab label="Banlist management" {...a11yProps(1)} />
+
+          </Tabs>
+        </Grid>
+
+        <Grid item xs={10} >
           <TabPanel value={value} index={0}>
 
-            <Typography variant='h5' style={{paddingLeft: '3%'}} >
+            <Typography variant='h5' style={{ paddingLeft: '3%' }} >
               Change Password
+              <br />
+              <br />
             </Typography>
 
             <Grid container spacing={3} justify='center'>
-            <Grid item xs={12}>
-              <Typography gutterBottom>
-                <TextField id="outlined-basic" label="Old Password" variant="outlined" fullWidth={true} size="small" onChange={handleChange} name="password" type="password"/>
-              </Typography>
+              <Grid item xs={5}>
+                <Typography gutterBottom>
+                  <TextField id="outlined-basic" label="Old Password" variant="outlined" fullWidth={true} size="small" onChange={passwordChange} name="password" type="password" />
+                </Typography>
 
-              <Typography gutterBottom>
-                <TextField id="outlined-basic" label="New Password" variant="outlined" fullWidth={true} size="small" onChange={handleChange} name="password" type="password"/>
-              </Typography>
+                <Typography gutterBottom>
+                  <TextField id="outlined-basic" label="New Password" variant="outlined" fullWidth={true} size="small" onChange={passwordChange} name="password" type="password" />
+                </Typography>
 
-              <Typography gutterBottom>
-                <TextField id="outlined-basic" label="Confirm New Password" variant="outlined" fullWidth={true} size="small" onChange={handleChange} name="password" type="password"/>
-              </Typography>
+                <Typography gutterBottom>
+                  <TextField id="outlined-basic" label="Confirm New Password" variant="outlined" fullWidth={true} size="small" onChange={passwordChange} name="password" type="password" />
+                </Typography>
 
-              <Button variant="contained" color="primary" fullWidth={true}>
-                Confirm
+                <Button variant="contained" color="primary" fullWidth={true}>
+                  Confirm
               </Button>
-            
-            </Grid>
-            </Grid>
-           </TabPanel>
 
-          
+              </Grid>
+            </Grid>
+
+          </TabPanel>
+
+
           <TabPanel value={value} index={1}>
-             <Grid container spacing={3} justify='center'>
-               <Grid item xs={12}>
 
-                  <Typography variant='h5' style={{paddingLeft: '3%'}} >
-                    Banlist management
-                  </Typography>
-                  
-                  <Chip
-                  icon={<FaUserCircle />}
-                  label="Anna"
-                  onClick={handleClick}
-                  onDelete={handleDelete}
-                  />
-                  <Divider />
+            <Typography variant='h5' style={{ paddingLeft: '3%' }} >
+              Banlist management
+              <br />
+              <br />
+            </Typography>
 
-                  <Chip
-                  icon={<FaUserCircle />}
-                  label="Becky"
-                  onClick={handleClick}
-                  onDelete={handleDelete}
-                  />
-                  <Divider />
+            <Grid container spacing={3} justify='center'>
+              <Grid item xs={5}>
+                <List dense={dense}>
+                  {chipData.map((data) => (
 
-                  <Chip
-                  icon={<FaUserCircle />}
-                  label="Charlie"
-                  onClick={handleClick}
-                  onDelete={handleDelete}
-                  />
-                  <Divider />
-
-                  <Chip
-                  icon={<FaUserCircle />}
-                  label="Denny"
-                  onClick={handleClick}
-                  onDelete={handleDelete}
-                  />
-                  <Divider />
-
-                  <Chip
-                  icon={<FaUserCircle />}
-                  label="Flora"
-                  onClick={handleClick}
-                  onDelete={handleDelete}
-                  />
-               </Grid>
-             </Grid>
-           </TabPanel>
-          
-         </Grid>
-       </Grid>
-
+                    <ListItem>
+                      <ListItemAvatar>
+                        <Avatar>
+                          <FaUserCircle />
+                        </Avatar>
+                      </ListItemAvatar>
+                      <ListItemText
+                        primary={data.label}
+                      />
+                      <ListItemSecondaryAction>
+                        <IconButton edge="end" aria-label="delete" onClick={handleDelete(data)}>
+                          <DeleteIcon />
+                        </IconButton>
+                      </ListItemSecondaryAction>
+                    </ListItem>
+                  ))
+                  }
+                </List>
+              </Grid>
+            </Grid>
+          </TabPanel>
+        </Grid>
+      </Grid>
     </div>
   );
 }
