7bc4c9059d4110ac20548bd49f275cd7235e2551
Jiawen <jiawen.huang0323@gmail.com>
2020-11-01 23:18:01 +0800

    add try/catch

--- filmfinder/src/main/java/com/matrix/filmfinder/controller/RateController.java
+++ filmfinder/src/main/java/com/matrix/filmfinder/controller/RateController.java
@@ -3,11 +3,14 @@ package com.matrix.filmfinder.controller;
 import com.matrix.filmfinder.dao.RateRepository;
 import com.matrix.filmfinder.model.Rate;
 import com.matrix.filmfinder.model.User;
+import org.codehaus.jackson.JsonProcessingException;
+import org.codehaus.jackson.map.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping(path="/rate")
@@ -21,17 +24,28 @@ public class RateController {
 
 //    add
     @PostMapping(path="/addrate")
-    public String addRate(
+    public ResponseEntity<String> addRate(
             @RequestParam(name = "uid") User uid,
             @RequestParam(name = "movie_id") Integer movie_id,
-            @RequestParam(name = "rating") Integer rating)
-    {
+            @RequestParam(name = "rating") Integer rating) throws IOException {
         Rate rate = new Rate();
         rate.setUser(uid);
         rate.setMovie_id(movie_id);
         rate.setRating(rating);
         rateRepository.save(rate);
-        return "Rate successfully";
+        String rateJson = "";
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            rateJson = objectMapper.writeValueAsString(rate);
+        } catch(JsonProcessingException e) {
+            return new ResponseEntity<>(
+                    "rate json generation error in addrate",
+                    HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return new ResponseEntity<>(
+                rateJson,
+                HttpStatus.OK
+        );
     }
 
     //    update
