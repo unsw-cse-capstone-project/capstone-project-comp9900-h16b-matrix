975effb3299f5d6e2ff52f9296659eb9817ad5f9
charles0801 <wqypoiu@live.cn>
2020-11-05 10:44:27 +0800

    add constraints to rate and wishlist, debugged the wishlist function

--- filmfinder/src/main/java/com/matrix/filmfinder/dao/MovieRepository.java
+++ filmfinder/src/main/java/com/matrix/filmfinder/dao/MovieRepository.java
@@ -1,10 +1,13 @@
 package com.matrix.filmfinder.dao;
 
 import com.matrix.filmfinder.model.Movie;
+import com.matrix.filmfinder.model.User;
 import com.matrix.filmfinder.model.Wishlist;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface MovieRepository extends JpaRepository<Movie, Integer> {
@@ -19,6 +22,10 @@ public interface MovieRepository extends JpaRepository<Movie, Integer> {
     Movie getMovieByTmdb_id(String tmdb_id);
 
     List<Movie> findByIdIn(List<Wishlist> wishlists);
+    @Query(
+            value = "Select m from Movie m inner join Wishlist w on w.user = ?1 and m = w.movie"
+    )
+    List<Movie> findMoviesByUserFromWishlists(User u);
 //    @Query(
 //            value = "select c from Movie m left join Comment c where m.tmdb_id = ?1"
 //    )
