6832a06326f815f93ae9787b1b6030cce4150f71
charles0801 <wqypoiu@live.cn>
2020-11-05 12:49:33 +0800

    CommentController delete method modified, wishlistRepo debugged

--- filmfinder/src/main/java/com/matrix/filmfinder/dao/WishlistRepository.java
+++ filmfinder/src/main/java/com/matrix/filmfinder/dao/WishlistRepository.java
@@ -1,17 +1,27 @@
 package com.matrix.filmfinder.dao;
 
+import com.matrix.filmfinder.message.WishlistMessage;
 import com.matrix.filmfinder.model.Movie;
 import com.matrix.filmfinder.model.User;
 import com.matrix.filmfinder.model.Wishlist;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface WishlistRepository extends JpaRepository<Wishlist, Integer> {
 //    List<Wishlist> getWishlistsByUid(Integer uid);
-    List<Wishlist> getWishlistsByUser(User user);
+//    List<Wishlist> getWishlistsByUser(User user);
 
 //    List<Wishlist> findByUidAndMovie(Integer uid, Integer movie_id);
 //    Wishlist findByUidAndMovie_id(Integer uid, Integer movie_id);
     Wishlist getWishlistByUserAndMovie(User u, Movie m);
+
+    @Query(
+            value = "select w.id as wishlist_id, m.tmdb_id as tmdb_id, m.title as title, m.description as description, m.poster as poster " +
+                    "from Movie m, Wishlist w " +
+                    "where m = w.movie " +
+                    "and w.user = ?1"
+    )
+    List<WishlistMessage> getWishListsByUser(User user);
 }
