754dbed271ffb26ddb9d56f2666ce7ed0f129ae9
Jiawen <jiawen.huang0323@gmail.com>
2020-11-11 02:20:48 +0800

    Delete
    Delete Genre table and table which connect Genre table and movie table

--- filmfinder/src/main/java/com/matrix/filmfinder/dao/Film_GenreRepository.java
+++
@@ -1,7 +0,0 @@
-package com.matrix.filmfinder.dao;
-
-import com.matrix.filmfinder.model.Film_Genre;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface Film_GenreRepository extends JpaRepository<Film_Genre, Integer> {
-}
--- filmfinder/src/main/java/com/matrix/filmfinder/dao/GenreRepository.java
+++
@@ -1,7 +0,0 @@
-package com.matrix.filmfinder.dao;
-
-import com.matrix.filmfinder.model.Genre;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GenreRepository extends JpaRepository<Genre, Integer> {
-}
