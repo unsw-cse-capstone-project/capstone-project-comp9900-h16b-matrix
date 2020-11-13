5d8b0be2ef21dc252f48eb03aa8d2d6aecad6607
Jiawen <jiawen.huang0323@gmail.com>
2020-11-13 15:53:48 +0800

    Review table
    Add a function about getting all reviews by using movie_id.

--- filmfinder/src/main/java/com/matrix/filmfinder/dao/ReviewRepository.java
+++ filmfinder/src/main/java/com/matrix/filmfinder/dao/ReviewRepository.java
@@ -5,10 +5,14 @@ import com.matrix.filmfinder.model.Review;
 import com.matrix.filmfinder.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ReviewRepository extends JpaRepository<Review, Integer> {
     Review getByUserAndMovie(User user, Movie movie);
 
     Review getByIdAndUser(int review, User user);
 
     Review getReviewById(int review_id);
+
+    List<Review> findReviewsByMovie(Movie movie);
 }
