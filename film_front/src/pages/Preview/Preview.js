e9f0232c9437372f7589eb1ff652c8aaea2bf1b9
Jarvanstyle <jarvanstyle@gmail.com>
2020-11-13 17:08:46 +1100

    preview

---
+++ film_front/src/pages/Preview/Preview.js
@@ -0,0 +1,127 @@
+import { Button, Grid, Input, Paper, TextField } from "@material-ui/core";
+import React, { useEffect, useState } from "react";
+import NavBar from "../NavBar/NavBar";
+import BraftEditor from "./component/Prestyle";
+import { Link } from "react-router-dom";
+import {
+  createMuiTheme,
+  responsiveFontSizes,
+  ThemeProvider,
+} from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+
+const myDate = new Date();
+const jwt = require("jwt-simple");
+
+let decoded;
+const token = localStorage.getItem("userInfo");
+if (token) {
+  decoded = jwt.decode(token, process.env.REACT_APP_TOKEN_SECRET);
+} else {
+  window.location.href = `${process.env.REACT_APP_HOST_URL}`;
+}
+
+const theme = createMuiTheme({
+  typography: {
+    title: {
+      fontSize: 100,
+    },
+    title1: {
+      fontSize: 120,
+    },
+    body1: {
+      fontWeight: 500,
+    },
+    button: {
+      fontStyle: "italic",
+    },
+  },
+});
+
+export default function Preview(props) {
+  var url = window.location.href;
+  var arrUrl = url.split("/");
+  const movieid = arrUrl[arrUrl.length - 1];
+  const reviewKey = decoded.id.toString() + "@" + movieid;
+  const reviewJson = localStorage.getItem(reviewKey);
+  const reviewValue = JSON.parse(reviewJson);
+  console.log(reviewKey, reviewValue);
+  const [Date, setDate] = useState(myDate.toLocaleDateString());
+  const [open, setOpen] = useState(false);
+  const [SignupOpen, SignupsetOpen] = useState(false);
+  const [logout, setLogout] = useState(false);
+  const rederLogout = () => setLogout(!logout);
+  const handleSignupOpen = () => {
+    SignupsetOpen(true);
+    handleClose();
+  };
+  const SignupClose = () => {
+    SignupsetOpen(false);
+  };
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const removeSet = (uid) => {
+    localStorage.removeItem(uid);
+  };
+  return (
+    <Grid container justify="center" spacing={3}>
+      <Grid item xs={12}>
+        <NavBar
+          handleClose={handleClose}
+          open={open}
+          handleClickOpen={handleClickOpen}
+          SignupClose={SignupClose}
+          SignupOpen={SignupOpen}
+          handleSignupOpen={handleSignupOpen}
+          rederLogout={rederLogout}
+        />
+      </Grid>
+      <Grid item xs={10}>
+        <Grid container justify="flex-end" spacing={3}>
+          <Grid item>
+            <Button
+              variant="contained"
+              color="primary"
+              component={Link}
+              to={{ pathname: `/editReview/${movieid}` }}
+            >
+              Edit Preview
+            </Button>
+            :
+          </Grid>
+          <Grid item>
+            <Button
+              variant="contained"
+              color="primary"
+              //   onClick={localStorage.removeItem(reviewKey)}
+              component={Link}
+              to={{ pathname: `/movieDetail/${movieid}` }}
+            >
+              Send
+            </Button>
+            :
+          </Grid>
+          <Grid item xs={12}>
+            <ThemeProvider theme={theme}>
+              <Typography variant="h3">
+                {reviewValue ? reviewValue.title : null}
+              </Typography>
+              <Typography variant="title1">{reviewKey}</Typography>
+              <Typography>body1</Typography>
+              <div
+                dangerouslySetInnerHTML={{
+                  __html: reviewValue ? reviewValue.content : null,
+                }}
+              ></div>
+            </ThemeProvider>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
+  );
+}
--- film_front/src/pages/Review/ReviewEdit.js
+++ film_front/src/pages/Review/ReviewEdit.js
@@ -2,6 +2,7 @@ import { Button, Grid, Input, Paper, TextField } from "@material-ui/core";
 import React, { useEffect, useState } from "react";
 import NavBar from "../NavBar/NavBar";
 import BraftEditor from "./component/editor";
+import { Link } from "react-router-dom";
 
 import InputAdornment from "@material-ui/core/InputAdornment";
 const jwt = require("jwt-simple");
@@ -13,7 +14,26 @@ export default function ReviewEdit(props) {
   } else {
     window.location.href = `${process.env.REACT_APP_HOST_URL}`;
   }
-  const [values, setValues] = useState({ title: "", content: "" });
+
+  var url = window.location.href;
+  var arrUrl = url.split("/");
+  const movieid = arrUrl[arrUrl.length-1];
+  let t = "";
+  let c = "";
+  let reviewKey = decoded.id.toString()+"@"+movieid;
+
+  const reviewJson = localStorage.getItem(reviewKey);
+  const reviewValue = JSON.parse(reviewJson);
+  console.log(reviewValue, reviewJson);
+  if (reviewValue){
+    c = reviewValue.content;
+    t = reviewValue.title;
+  }
+  const [values, setValues] = useState({ title: t, content: c });
+
+  console.log(values)
+
+ 
   const [open, setOpen] = useState(false);
   const [SignupOpen, SignupsetOpen] = useState(false);
   const [logout, setLogout] = useState(false);
@@ -32,9 +52,18 @@ export default function ReviewEdit(props) {
     setOpen(false);
   };
   const handleContent = (e) => {
-    setValues({ ...values, content: e });
-    console.log(values,'values')
+      setValues({ ...values, content: e });
+      console.log(values,'values')
   };
+  const handleSet = (uid, review) => {
+    var myJson = JSON.stringify(review);
+    console.log(uid,review);    
+    localStorage.setItem(uid, myJson);
+  }
+  // const removeSet = (uid) => {
+  //   localStorage.removeItem(uid);
+  // }
+  
   return (
     <Grid container justify="center" spacing={3}>
       <Grid item xs={12}>
@@ -51,20 +80,52 @@ export default function ReviewEdit(props) {
       <Grid item xs={10}>
           <Grid container justify="flex-end" spacing={3}>
               <Grid item>
-                  <Button variant="contained" color="primary">
+                  <Button variant="contained" 
+                  color="primary"
+                  // onClick={localStorage.removeItem(reviewKey)}
+                  component={Link}
+                  to={{ pathname: `/movieDetail/${movieid}` }}>
                       Send
                   </Button>
                   </Grid>
                   <Grid item>
-                  <Button variant="outlined" color="primary">
+                  {decoded?
+                      <Button variant="outlined" 
+                          color="primary"
+                          onClick={()=>handleSet(reviewKey, values)}
+                          component={Link}
+                          to={{ pathname: `/Preview/${movieid}` }}
+                          >
                       Preview
-                  </Button>
+                      </Button>:
+                      <Button
+                          size="large"
+                          color="primary"
+                          onClick={handleClickOpen}>
+                      Preview
+                      </Button>
+                  }
               </Grid>
           </Grid>
 
       </Grid>
       <Grid item xs={6}>
+        {reviewValue?
         <TextField
+        id="standard-start-adornment"
+        fullWidth
+        value={values.title}
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">Title:</InputAdornment>
+          ),
+        }}
+        onChange={(e) => {
+          setValues({ ...values, title: e.target.value });
+        }}
+        //reviewValue = {NaN}
+      />:
+      <TextField
           id="standard-start-adornment"
           fullWidth
           InputProps={{
@@ -76,15 +137,25 @@ export default function ReviewEdit(props) {
             setValues({ ...values, title: e.target.value });
           }}
         />
+      }
       </Grid>
       <Grid item xs={10}>
-        <Paper elevation={3}>
+         {reviewValue?
+          <Paper elevation={3}>
+          <BraftEditor
+            handleContent={handleContent}
+            content={reviewValue.content}
+            id={undefined}
+          />
+        </Paper>: 
+          <Paper elevation={3}>
           <BraftEditor
             handleContent={handleContent}
             content={values.content}
             id={undefined}
-          />
+        />
         </Paper>
+        }
       </Grid>
     </Grid>
   );
