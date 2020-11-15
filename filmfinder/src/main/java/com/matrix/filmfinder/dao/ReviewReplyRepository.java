161ba27edcf4181699968ca88074fe26332bed6d
charles0801 <wqypoiu@live.cn>
2020-11-16 03:46:20 +0800

    ReviewReplyRepository bug fixed

--- filmfinder/src/main/java/com/matrix/filmfinder/dao/ReviewReplyRepository.java
+++ filmfinder/src/main/java/com/matrix/filmfinder/dao/ReviewReplyRepository.java
@@ -17,7 +17,7 @@ public interface ReviewReplyRepository extends JpaRepository<ReviewReply, Intege
                     "from review_reply r left join " +
                     "(select * from blacklist b where b.user_id = ?2) as b " +
                     "on r.reply_user_id = b.banned_user_id or r.review_user_id = b.banned_user_id " +
-                    "where b.banned_user_id is null and r.id = ?1 "
+                    "where b.banned_user_id is null and r.review_id = ?1 "
     )
     List<ReviewReply> findByReviewWithBlacklistFilter(Review review, User user);
 }
